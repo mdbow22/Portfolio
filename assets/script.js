d4ccdc289b321210fa99595d34634916c9c7cb2e
mdbow22 <mdbow22@gmail.com>
2021-09-17 21:14:54 -0400

    Created animated menu effect

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,19 @@
 //Selectors
 
-let menuBtn = document.querySelector('.menu-btn');
+/* let menuBtn = document.querySelector('.menu-btn'); */
 let hamburger = document.querySelector('.fa-bars');
 let times = document.querySelector('.fa-times');
+let menu = document.querySelector('nav');
+let body = document.getElementById('site-body');
 
 //Event Listeners
 
-menuBtn.addEventListener('click',function() {
-    hamburger.classList.toggle('off');
-    times.classList.toggle('off');
+hamburger.addEventListener('click',function() {
+    menu.classList.remove('off');
+    body.style.overflow = 'hidden';
 });
+
+times.addEventListener('click', function() {
+    menu.classList.add('off');
+    body.style.overflow = 'visible';
+})
